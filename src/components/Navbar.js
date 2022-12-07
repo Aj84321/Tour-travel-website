5f3ae684aa749733165dc2267a355466ef2bd4c3
Anand Jain <105570110+Aj84321@users.noreply.github.com>
2022-12-07 18:59:05 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,23 @@ import {MenuItems} from "./MenuItems";
 // import { Link } from "react-router-dom";
 
 class Navbar extends Component {
+  state = {clicked: false};
+  handleClick = () =>{
+    this.setState({clicked: !this.state.clicked})
+  }
   render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">Trippy</h1>
-        <div  className="menu-icons">
-        <i className="fas fa-bars"></i>
-        <i className="fas fa-times"></i> 
+        <div className="menu-icons" onClick=
+        {this.handleClick}>
+        <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
     
 
-        <ul className="nav-menu">
-          {MenuItems.map((item, index)=>{
+        <ul className={this.state.clicked ? 
+          "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
             return (
               <li key={index}>
               <a className={item.cName}href="/">
